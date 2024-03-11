4fc067993717862849076aeb150123e88638fa6f
Clement Argentin <clement.argentin@sii.fr>
2024-03-11 10:40:43 +0100

    remove special tokens

--- src/prompts.py
+++ src/prompts.py
@@ -1,11 +1,10 @@
 PROMPTS = {
     "RAG_PDF": (
-        "<s> I want you to answer a question based on a chunk of a"
-        "retrieved file that I will give you. If you don't find the answer in"
-        " the text that I give you, answer: 'I don't find anything in the"
-        "corresponding text'. [INST] First write the page number from the PDF:"
-        "{page_number} if there is one, then answer the question: {question}"
-        " with the text: {content}. [/INST]</s>"
+        "You are a helpful bot who answer a question: {question} based on a "
+        "chunk of a retrieved file: {content}, where you write the page number"
+        " of the PDF: {page_number} if there is one. If you don't find the"
+        " answer in the text that I give you, answer: 'I don't find anything "
+        "in the corresponding text'"
     ),
     "RAG_PDFs_data": (
         "<s> I want you to answer a question based on information"
